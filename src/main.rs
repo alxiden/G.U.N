9637f69abaf07ef357fd8eac504bfd9643a5b979
Daniel <github.com.rectify325@passmail.net>
2025-08-29 08:54:49 +0100

    Add web scraping support for acronym expansion
    Added 'reqwest' and 'scraper' dependencies to Cargo.toml and implemented web scraping logic in src/acronyms.rs and src/main.rs to fetch acronym expansions from the web. This enhances the application's ability to provide more comprehensive acronym definitions.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::fs;
 use std::fs::File;
 use std::io::Write;
 
-fn txt_reader(acronyms: &HashMap<&str, &str>) {
+fn txt_reader(acronyms: &HashMap<String, String>) {
     let mut file_path = String::new();
     println!("Please enter the file path:");
     io::stdin().read_line(&mut file_path).expect("Failed to read line");
@@ -17,14 +17,14 @@ fn txt_reader(acronyms: &HashMap<&str, &str>) {
     for (acronym, meaning) in acronyms.iter() {
         let pattern = format!(r"\b{}\b", regex::escape(acronym)); //Creates the regex pattern
         let re = Regex::new(&pattern).unwrap(); //Compiles the regex pattern
-        input_string = re.replace_all(&input_string, *meaning).to_string(); //Replaces the acronym with its meaning
+        input_string = re.replace_all(&input_string, meaning).to_string(); //Replaces the acronym with its meaning
     }
 
     let mut output_file = File::create("output.txt").expect("Failed to create output file");
     output_file.write_all(input_string.as_bytes()).expect("Failed to write to output file");
 }
 
-fn paste_text(acronyms: &HashMap<&str, &str>) {
+fn paste_text(acronyms: &HashMap<String, String>) {
     let mut input_string = String::new(); //Creates a new user input variable
     println!("Paste the text to be de-acronymed:"); // Prompt the user for the text
     io::stdin().read_line(&mut input_string).expect("Failed to read line"); //reads the input and looks for errors
@@ -32,13 +32,13 @@ fn paste_text(acronyms: &HashMap<&str, &str>) {
     for (acronym, meaning) in acronyms {
         let pattern = format!(r"\b{}\b", regex::escape(acronym)); //Creates the regex pattern
         let re = Regex::new(&pattern).unwrap(); //Compiles the regex pattern
-        input_string = re.replace_all(&input_string, *meaning).to_string(); //Replaces the acronym with its meaning
+        input_string = re.replace_all(&input_string, meaning).to_string(); //Replaces the acronym with its meaning
     }
 
     println!("Your acronym free text: {}", input_string.trim()); // Print the de-acronymed text
 }
 
-fn menu(acronyms: &HashMap<&str, &str>) {
+fn menu(acronyms: &HashMap<String, String>) {
     println!("Please select an option:");
     println!("1. Read text from file");
     println!("2. Paste text");
